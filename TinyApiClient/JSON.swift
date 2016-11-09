2b8bfb60759be8922b8e3b28b1c0d16ae8bb878f
Daniel Lozano <danlozano@gmail.com>
2016-11-09 15:57:13 -0600

    added xcode project

---
+++ TinyApiClient/JSON.swift
@@ -0,0 +1,9 @@
+//
+//  JSON.swift
+//  TinyApiClient
+//
+//  Created by Daniel Lozano on 11/9/16.
+//  Copyright © 2016 Daniel Lozano. All rights reserved.
+//
+
+import Foundation
---
+++ TinyApiClient/TinyApiClient.h
@@ -0,0 +1,19 @@
+//
+//  TinyApiClient.h
+//  TinyApiClient
+//
+//  Created by Daniel Lozano on 11/9/16.
+//  Copyright © 2016 Daniel Lozano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyApiClient.
+FOUNDATION_EXPORT double TinyApiClientVersionNumber;
+
+//! Project version string for TinyApiClient.
+FOUNDATION_EXPORT const unsigned char TinyApiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyApiClient/PublicHeader.h>
+
+
